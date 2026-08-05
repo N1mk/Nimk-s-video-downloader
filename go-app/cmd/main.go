a8553ba813785cb67e188ae699ee6fcb6c2a4130
N1mk <mikha.chebotaryov@gmail.com>
2026-08-05 19:26:33 +0300

    cors fixed

--- go-app/cmd/main.go
+++ go-app/cmd/main.go
@@ -97,7 +97,12 @@ func main() {
 	r.Post("/update", h.PostDownloaderUpdate)
 
 	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		w.WriteHeader(http.StatusOK)
 	})
